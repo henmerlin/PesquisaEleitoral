24aa0f42ed4f2e31500e6bddc8e623b5575c2298
henmerlin <henmerlin@gmail.com>
2016-09-14 15:01:35 -0300

    Reports

--- PesquisaEleitoral/Apuracao/Apuracao.aspx.designer.cs
+++ PesquisaEleitoral/Apuracao/Apuracao.aspx.designer.cs
@@ -20,5 +20,23 @@ namespace PesquisaEleitoral.Apuracao {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::Microsoft.Reporting.WebForms.ReportViewer ReportViewer1;
+        
+        /// <summary>
+        /// ds1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ObjectDataSource ds1;
+        
+        /// <summary>
+        /// ReportViewer2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Microsoft.Reporting.WebForms.ReportViewer ReportViewer2;
     }
 }
